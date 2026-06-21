26eb0ff4e2dbefb9cf2230c6650879233878f4a7
deepasharma843580-art <deepasharma843580@gmail.com>
2026-06-21 19:11:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+TELEGRAM_BOT_TOKEN = "8870485051:AAHJUScgMJJB4ItiqY9WHBFmVtRrHh9hSkk"
+
+EMAIL_ACCOUNTS = [
+    {
+        "email": "meenaserver123280@gmail.com",
+        "password": "knbf bmdf ymzy rgly",
+        "telegram_chats": ["-1004323799035"]
+    }
+]
+
+CHECK_INTERVAL = 30
